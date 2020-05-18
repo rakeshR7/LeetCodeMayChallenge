ce3265643521d23cda70455240c2e40d083faeed
Rakesh <36699589+rakeshR7@users.noreply.github.com>
2020-05-18 15:12:30 -0500

    Create PermutationString.py

---
+++ PermutationString.py
@@ -0,0 +1,29 @@
+"""
+Given two strings s1 and s2, write a function to return true if s2 contains the permutation of s1. In other words, one of the first string's permutations is the substring of the second string.
+
+ 
+
+Example 1:
+
+Input: s1 = "ab" s2 = "eidbaooo"
+Output: True
+Explanation: s2 contains one permutation of s1 ("ba").
+Example 2:
+
+Input:s1= "ab" s2 = "eidboaoo"
+Output: False
+"""
+
+from collections import Counter
+class Solution:
+    def checkInclusion(self, s1: str, s2: str) -> bool:
+        l = len(s1)
+        cs1 = Counter(s1)
+        print(cs1)
+        cs2 = dict()
+        for i in range(len(s2)):
+            cs2[1] = Counter(s2[i:(i+l)])
+            if(cs2[1] == cs1):
+                return True
+            
+        return False
